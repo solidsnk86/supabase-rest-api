1982bd12d9fdced28fb2200ff13c172d133205c3
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-02-07 07:01:49 -0300

    origin added

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,6 +4,7 @@ const ACCEPTED_ORIGINS = [
   'https://calcagni-gabriel.vercel.app',
   'https://neo-wifi.vercel.app',
   'https://double-commit.vercel.app',
+  'http://localhost:5173/'
 ]
 
 const corsMiddleware = ({ aceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
