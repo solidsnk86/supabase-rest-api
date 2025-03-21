fa741835d42a7131a5ec679b91682cdaceb57d67
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-21 00:48:23 -0300

    remove tst

--- middleware/cors.js
+++ middleware/cors.js
@@ -12,9 +12,6 @@ const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
       if (acceptedOrigins.includes(origin)) {
         return callback(null, true)
       }
-      if (!acceptedOrigins.includes(origin)) {
-        return callback(null, true)
-      }
 
       return callback(new Error('Not allowed by CORS: 🐶'))
     },
