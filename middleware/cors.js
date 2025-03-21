2acd63ed5ab5c8f320dd1ceca920d0426f8af86c
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-21 16:25:11 -0300

    test api

--- middleware/cors.js
+++ middleware/cors.js
@@ -12,6 +12,9 @@ const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
       if (acceptedOrigins.includes(origin)) {
         return callback(null, true)
       }
+      if (!acceptedOrigins.contains(origin)) {
+        return callback(null, true)
+      }
 
       return callback(new Error('Not allowed by CORS: 🐶'))
     },
