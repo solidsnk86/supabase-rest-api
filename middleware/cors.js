eb461a3a1de0d46e6d29b5e6855ee34800e3c55e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-21 16:54:33 -0300

    remove origins

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,16 +4,12 @@ const ACCEPTED_ORIGINS = [
   'https://calcagni-gabriel.vercel.app',
   'https://neo-wifi.vercel.app',
   'https://double-commit.vercel.app',
-  'http://localhost:3000'
 ]
 
 const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
  return cors({
     origin: (origin, callback) => {
       if (acceptedOrigins.includes(origin)) {
-        return callback(null, true)
-      }
-      if (!acceptedOrigins.includes(origin)) {
         return callback(null, true)
       }
 
